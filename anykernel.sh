f443b9a9949d95244466b98bbd23af714673b521
fadlyas07 <fadlyardhians@gmail.com>
2020-01-23 05:37:44 +0700

    mod :v
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=GREENFORCE-FAMILY kernel by @Mhmmdfas
+kernel.string=Hoi Bro!
 do.devicecheck=1
 do.modules=0
 do.systemless=0
