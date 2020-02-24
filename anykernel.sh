e4a8ab6ff5f762183b6d526bc2ac76e0aee55328
fadlyas07 <fadlyardhians@gmail.com>
2020-02-24 09:30:45 +0700

    Initial Anykernel for miui kernel

--- anykernel.sh
+++ anykernel.sh
@@ -1,4 +1,4 @@
-# AnyKernel3 Ramdisk Mod Script
+# AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
@@ -6,46 +6,37 @@
 properties() { '
 kernel.string=Hoi Bro!
 do.devicecheck=1
-do.modules=0
-do.systemless=0
+do.modules=1
 do.cleanup=1
-do.cleanuponabort=1
+do.cleanuponabort=0
 device.name1=rolex
 device.name2=redmi4a
 device.name3=riva
 device.name4=redmi5a
-device.name5=vince
-device.name6=redmi5plus
-device.name7=lavender
-device.name8=redminote7
-supported.versions=
-supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=auto;
-is_slot_device=auto;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+. /tmp/anykernel/tools/ak2-core.sh;
 
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+# Messages
+ui_print "Kernel is built using GCC Toolchain For MIUI OK";
+ui_print "                This kernel Pure               ";
+ui_print "-----------------------------------------------";
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
 # end ramdisk changes
 
 write_boot;
+
 ## end install
 
